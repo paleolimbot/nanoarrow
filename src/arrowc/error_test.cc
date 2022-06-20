f04aba2781251ca56032b1b3bc2537f3ba049a3a
Dewey Dunnington <dewey@fishandwhistle.net>
2022-06-20 14:26:58 -0300

    return error from ArrowErrorSet on buffer overrun and encoding error

--- src/arrowc/error_test.cc
+++ src/arrowc/error_test.cc
@@ -15,6 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
+#include <cerrno>
 #include <cstring>
 #include <string>
 
@@ -37,7 +38,9 @@ TEST(ErrorTest, ErrorTestSetOverrun) {
   }
   big_error[2047] = '\0';
 
-  EXPECT_EQ(ArrowErrorSet(&error, "%s", big_error), ARROWC_OK);
-
+  EXPECT_EQ(ArrowErrorSet(&error, "%s", big_error), ERANGE);
   EXPECT_EQ(std::string(ArrowErrorMessage(&error)), std::string(big_error, 1022));
+
+  wchar_t bad_string[] = {0xFFFF, 0};
+  EXPECT_EQ(ArrowErrorSet(&error, "%ls", bad_string), EINVAL);
 }
