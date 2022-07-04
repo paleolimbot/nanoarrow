3c62348fd268ac349a249bfb37a01af156e3c1f3
Dewey Dunnington <dewey@fishandwhistle.net>
2022-07-04 12:54:47 -0300

    use a more predictable allocator for tests

--- src/nanoarrow/buffer_test.cc
+++ src/nanoarrow/buffer_test.cc
@@ -23,11 +23,45 @@
 
 #include "nanoarrow/nanoarrow.h"
 
+// This test allocator guarantees that allocator->reallocate will return
+// a new pointer so that we can test when reallocations happen whilst
+// building buffers.
+static uint8_t* TestAllocatorAllocate(struct ArrowBufferAllocator* allocator,
+                                      int64_t size) {
+  return reinterpret_cast<uint8_t*>(malloc(size));
+}
+
+static uint8_t* TestAllocatorReallocate(struct ArrowBufferAllocator* allocator,
+                                        uint8_t* ptr, int64_t old_size,
+                                        int64_t new_size) {
+  uint8_t* new_ptr = TestAllocatorAllocate(allocator, new_size);
+
+  int64_t copy_size = std::min<int64_t>(old_size, new_size);
+  if (new_ptr != nullptr && copy_size > 0) {
+    memcpy(new_ptr, ptr, copy_size);
+  }
+
+  if (ptr != nullptr) {
+    free(ptr);
+  }
+
+  return new_ptr;
+}
+
+static void TestAllocatorFree(struct ArrowBufferAllocator* allocator, uint8_t* ptr,
+                              int64_t size) {
+  free(ptr);
+}
+
+static struct ArrowBufferAllocator test_allocator = {
+    &TestAllocatorAllocate, &TestAllocatorReallocate, &TestAllocatorFree, nullptr};
+
 TEST(BufferTest, BufferTestBasic) {
   struct ArrowBuffer buffer;
 
   // Init
   ArrowBufferInit(&buffer);
+  buffer.allocator = &test_allocator;
   EXPECT_EQ(buffer.data, nullptr);
   EXPECT_EQ(buffer.capacity_bytes, 0);
   EXPECT_EQ(buffer.size_bytes, 0);
@@ -68,7 +102,8 @@ TEST(BufferTest, BufferTestBasic) {
 
   // Transfer responsibility to another allocator
   struct ArrowBufferAllocator non_default_allocator;
-  memcpy(&non_default_allocator, ArrowBufferAllocatorDefault(), sizeof(struct ArrowBufferAllocator));
+  memcpy(&non_default_allocator, ArrowBufferAllocatorDefault(),
+         sizeof(struct ArrowBufferAllocator));
 
   EXPECT_EQ(ArrowBufferSetAllocator(&buffer, &non_default_allocator), NANOARROW_OK);
   EXPECT_NE(buffer.data, first_data);
@@ -83,7 +118,8 @@ TEST(BufferTest, BufferTestBasic) {
   EXPECT_EQ(buffer.size_bytes, 0);
 
   // Transfer allocator with empty buffer
-  EXPECT_EQ(ArrowBufferSetAllocator(&buffer, ArrowBufferAllocatorDefault()), NANOARROW_OK);
+  EXPECT_EQ(ArrowBufferSetAllocator(&buffer, ArrowBufferAllocatorDefault()),
+            NANOARROW_OK);
   EXPECT_EQ(buffer.data, nullptr);
   EXPECT_EQ(buffer.capacity_bytes, 0);
   EXPECT_EQ(buffer.size_bytes, 0);
@@ -93,11 +129,14 @@ TEST(BufferTest, BufferTestError) {
   struct ArrowBuffer buffer;
   ArrowBufferInit(&buffer);
   EXPECT_EQ(ArrowBufferReallocate(&buffer, std::numeric_limits<int64_t>::max()), ENOMEM);
-  EXPECT_EQ(ArrowBufferWriteChecked(&buffer, nullptr, std::numeric_limits<int64_t>::max()), ENOMEM);
+  EXPECT_EQ(
+      ArrowBufferWriteChecked(&buffer, nullptr, std::numeric_limits<int64_t>::max()),
+      ENOMEM);
 
   ASSERT_EQ(ArrowBufferWriteChecked(&buffer, "abcd", 4), NANOARROW_OK);
   struct ArrowBufferAllocator non_default_allocator;
-  memcpy(&non_default_allocator, ArrowBufferAllocatorDefault(), sizeof(struct ArrowBufferAllocator));
+  memcpy(&non_default_allocator, ArrowBufferAllocatorDefault(),
+         sizeof(struct ArrowBufferAllocator));
   buffer.capacity_bytes = std::numeric_limits<int64_t>::max();
 
   EXPECT_EQ(ArrowBufferSetAllocator(&buffer, &non_default_allocator), ENOMEM);
